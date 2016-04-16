82b1f1b1c64b7647be99de7b9ebd251c29274b56
wangxiaohu <wangxiaohu@ykse.com.cn>
2016-04-16 21:51:52 +0800

    首次提交

---
+++ src/com/wxh/text/TestApp.java
@@ -0,0 +1,11 @@
+package com.wxh.text;
+/**
+ * 测试
+ * @author wangxiaohu
+ * @version $Id: TestApp.java, v 0.1 2016年4月16日 下午9:50:35 wangxiaohu Exp $
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
