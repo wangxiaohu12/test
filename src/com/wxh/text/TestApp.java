84d4448b168fddeea31c16409b7074919a403451
Wangwu <Wangwu@qq.com>
2016-04-19 22:17:21 +0800

    删除

--- src/com/wxh/text/TestApp.java
+++ src/com/wxh/text/TestApp.java
@@ -5,24 +5,5 @@ package com.wxh.text;
  * @version $Id: TestApp.java, v 0.1 2016年4月16日 下午9:50:35 wangxiaohu Exp $
  */
 public class TestApp {
-    public static void main(String[] args) {
-        System.out.println("测试");
-        System.out.println("测试1");
-    }
-    private void test1(){
-    	System.out.println("test1");
-    }
-    private void test2(){
-    	System.out.println("test2");
-    }
-	private void test3(){
-	    System.out.println("test3");
-    }
-	private void test4(){
-		System.out.println("用户1编写");
-		System.out.println("用户2编写");
-		System.out.println("用户2编写的代码");
-		System.out.println("用户1编写的代码");
-		System.out.println("test4");
-    }
+
 }
