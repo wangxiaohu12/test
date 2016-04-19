68849cf52368120ef74c8b3e23cd399306cdc6dc
wangxh <wangxh>
2016-04-19 23:58:11 +0800

    no message

--- src/com/wxh/text/TestApp.java
+++ src/com/wxh/text/TestApp.java
@@ -7,8 +7,8 @@ package com.wxh.text;
 public class TestApp {
 	private void test1(){
 
-		String names="wangwu";
-		String names="wangwu";
+		String names="wangwu111";
+		String names="wangwu222";
 
 
 	}
